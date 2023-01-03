1fcbb28f14e6fa045d4cb9892be8f3178f867dc6
What Else? <example@example.com>
2023-01-03 18:19:59 +0100

    Update App component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  const websiteName = "Limitless"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  {{ websiteName }}
 </template>
 
 <style scoped>
