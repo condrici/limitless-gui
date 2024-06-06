d8538d746626d1b80f63e14a9fbb0a3c120aab8f
condrici <condrici@gmail.com>
2024-06-06 18:11:35 +0200

    Fix tailwind

--- vite/src/App.vue
+++ vite/src/App.vue
@@ -1,32 +1,34 @@
+<style>
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
+
 <script setup>
   const websiteName = "Limitless"
+
+// TEST CODE
+//
+// let scrapeFromUrl = encodeURIComponent('https://www.emag.ro/piscina-intex-28202np-cadru-metalic-305-x-76-cm-pompa-filtrare-inclusa-6941057400310/pd/DHLY9ZBBM/?ref=profiled_categories_home_1_2&provider=rec&recid=rec_50_c550f403274cccaa68b02d34293a188675ce7353fdfaa558c3dd46122ddb59ed_1691482523&scenario_ID=50')
+// let scrapeAlgorithm = 'emag_product_page'
+// let scrapeApiUrl = apiEndpoints.API_GATEWAY_ENDPOINT + '/scrape/' + scrapeFromUrl + '/' + scrapeAlgorithm
+
+// http.sendGetRequest('http://localhost:8083/api/v1.0/scrape/x/x').then(
+//     function (response) {
+//         console.log(response)
+//     }
+// )
+
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<template>
+<button class="btn btn-blue">
+  Button
+</button>
+</template>
--- vite/src/main.js
+++ vite/src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/main.css'
+
+// CSS
+import './assets/tailwind.css'
+
 import {http, apiEndpoints} from './features/Http';
+import TestComponent from './components/TestComponent.vue'
 
-
-// let scrapeFromUrl = encodeURIComponent('https://www.emag.ro/piscina-intex-28202np-cadru-metalic-305-x-76-cm-pompa-filtrare-inclusa-6941057400310/pd/DHLY9ZBBM/?ref=profiled_categories_home_1_2&provider=rec&recid=rec_50_c550f403274cccaa68b02d34293a188675ce7353fdfaa558c3dd46122ddb59ed_1691482523&scenario_ID=50')
-// let scrapeAlgorithm = 'emag_product_page'
-// let scrapeApiUrl = apiEndpoints.API_GATEWAY_ENDPOINT + '/scrape/' + scrapeFromUrl + '/' + scrapeAlgorithm
-
-// http.sendGetRequest('http://localhost:8083/api/v1.0/scrape/x/x').then(
-//     function (response) {
-//         console.log(response)
-//     }
-// )
+const app = createApp(App)
+app.mount('#app')
