7220b708dc01d71fde1687ec2bcdd6bdcce91faf
condrici <condrici@gmail.com>
2024-07-09 16:04:51 +0200

    Remove unnecessary lines

--- vite/src/main.js
+++ vite/src/main.js
@@ -4,8 +4,5 @@ import App from './App.vue'
 // CSS
 import './assets/tailwind.css'
 
-import {http, apiEndpoints} from './features/Http';
-import TestComponent from './components/TestComponent.vue'
-
 const app = createApp(App)
 app.mount('#app')
