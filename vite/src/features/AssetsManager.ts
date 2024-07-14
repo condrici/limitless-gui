4af74c07a5a59ab81f58f0f2d1802e61eec7506a
condrici <condrici@gmail.com>
2024-07-14 16:03:21 +0200

    Update AssetsManager.ts

--- vite/src/features/AssetsManager.ts
+++ vite/src/features/AssetsManager.ts
@@ -1,26 +1,16 @@
-import {http, apiEndpoints} from '../features/Http';
-
 class AssetsManager {
 
-    generateTablePagesData(totalPages) {
-        let tablePages = [];
-
+    generateTablePagesData(totalPages: number) {
+        let tablePages: any[] = [];
+        
         for (let i=0; i < totalPages; i++) {
             tablePages[i] = {};
             tablePages[i].number = i+1;
-            tablePages[i].isSelected = i === 0 ? true : false;
+            tablePages[i].isSelected = false;
         }
 
         return tablePages;
     }
 }
 
-export default class Zooom {
-    saySomething() {
-        console.log("bzzz")
-    }
-}
-
-let assetsManager = new AssetsManager();
-
-export {assetsManager, Zooom}
+export {AssetsManager}
